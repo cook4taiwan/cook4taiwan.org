78e90e0d1517b880d99298c8629bee23597404db
reitschule <himmelreitschule@gmail.com>
2017-02-13 19:54:50 +0800

    add pickcolor

--- main.js
+++ main.js
@@ -6,11 +6,33 @@ $(document).ready(function(){
     
     $("#button").click(function(){
         $("body").css("background-color",randomColor());
-        
-        
-        
+            
     });
     
+    
+    $("#red").click(function(){
+        $(".row").click(function(){
+            $(this).css("background-color","#cc0000"); 
+        })    
+    });
+    
+    $("#yellow").click(function(){
+        $(".row").click(function(){
+            $(this).css("background-color","#ffec00"); 
+        })    
+    });
+    
+    $("#blue").click(function(){
+        $(".row").click(function(){
+            $(this).css("background-color","#0000cc"); 
+        })    
+    });
+    
+    $("#white").click(function(){
+        $(".row").click(function(){
+            $(this).css("background-color","white"); 
+        })    
+    });
     
 })
 
