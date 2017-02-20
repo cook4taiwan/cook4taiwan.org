fbd181dd8fabc135bc7fef18e1d6aaa437b1d994
reitschule <himmelreitschule@gmail.com>
2017-02-20 20:21:16 +0800

    add sushi_animation

--- main.js
+++ main.js
@@ -42,6 +42,34 @@ $(document).ready(function(){
     $(".box").mouseleave(function(){
         $(this).css("box-shadow","none");
     });
+    
+    var animations = ["shake",
+                     "grow",
+                     "hooray",
+                     "spin",
+                     "hop"]
+    
+    function getRandomInt(max,min)  {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    
+    
+    
+    $(".sushi").click(function(){
+            
+        var animation = animations[getRandomInt(4,0)];
+        var sushi = this;
+        
+        $(sushi).addClass(animation);
+        
+        setTimeout(function(){
+            $(sushi).removeClass(animation);
+        },2100);
+    });
+    
+    
+    
+    
 });
 
 
