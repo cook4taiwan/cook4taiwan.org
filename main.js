ba61d25c8ce6f9a17de000412c4451c2e8ce03e3
reitschule <himmelreitschule@gmail.com>
2017-04-12 20:01:01 +0800

    add page001

--- main.js
+++ main.js
@@ -230,11 +230,11 @@ $(document).ready(function(){
                 $("#bdayBanner").text("Your birthday is " + months[month] + " " + day + ".");
                 
                 if(bday.getMonth() + 1 == "1" &&　bday.getDate() == "10") {
-                    $("#happyBday").text("You are Andy!");
+                    $("#happyBday").text("You are Shaune's boyfriend!");
                 }　else if (bday.getMonth() + 1 == "3" &&　bday.getDate() == "8") {
                     $("#happyBday").text("景皓是你");
                 } else if (bday.getMonth() + 1 == "6" &&　bday.getDate() == "3") {
-                    $("#happyBday").text("You are a nice 鈣.");
+                    $("#happyBday").text("Mary, you are a nice 鈣.");
                 } else if (bday.getMonth() + 1 == "8" &&　bday.getDate() == "27") {
                     $("#happyBday").text("創辦人，是你嗎?!!!");
                 } else if (bday.getMonth() + 1 == "7" &&　bday.getDate() == "7") {
@@ -243,21 +243,12 @@ $(document).ready(function(){
                     $("#happyBday").text("You are Andy's girlfriend!");
                 } else {
                     $("#happyBday").text("");
-                }
-            }
-            
-            
-            
-            
+                }:
+            }:
         };
-        
-        
+
     });
-    
-    
-    
-    
-    
+
 });
 
 
